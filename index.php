545c51bdd1291cf492581bcf1203711907bbd099
maplefortress <catbraces@gmail.com>
2020-04-23 00:37:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,59 +1,246 @@
-<?php
-  $page_name = "Dashboard";
-  include "partials/_header.php";
-  include "partials/_menu.php";
-?>
+<html>
 
-<main role="main" class="container-fluid">
+<head>
 
-  <div class="row">
-    <div class="col-sm">
-      <div class="contenttype-title">
-        <h3>SHITTY Posts</h3>
-        <a href="" class="side-text-button" data-toggle="modal" data-target="#add-content-modal"><i class="fas fa-plus-circle"></i> New post</a>
-      </div>
-      <div class="list-group">
-        <a href="#" class="list-group-item list-group-item-action">How to deal with savage chicken ?</a>
-        <a href="#" class="list-group-item list-group-item-action">Understand why the earth is flat in 5 minutes</a>
-        <a href="#" class="list-group-item list-group-item-action">Another article that no one will read</a>
-        <a href="#" class="list-group-item list-group-item-action">A new writer is here</a>
-        <a href="#" class="list-group-item list-group-item-action">First post on this blog</a>
-      </div>
-      <a href="list.php" class="contenttype-all-button float-right">View all posts <i class="fas fa-chevron-circle-right"></i></a>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>About | Maple Fortress</title>
+<meta name="robots" content="noindex, nofollow" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<style>
+  html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,a,abbr,acronym,address,big,del,dfn,img,ins,kbd,q,s,samp,small,strike,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline;}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section {display:block;}ol,ul{list-style:none;}blockquote,q{quotes:none;}blockquote:before,blockquote:after,q:before,q:after{content:'';content:none;}table{border-collapse:collapse;border-spacing:0;}
+  img {width: 100%;} 
+  
+  @font-face {
+    font-family:Fuji-Bold;
+    src:url(fonts/fuji-bold-webfont.woff2) format("woff2"),
+        url(fonts/fuji-bold-webfont.woff) format("woff"),;
+    font-style:normal;
+}
+     @font-face {
+    font-family:Fuji-Light;
+    src:url(fonts/fuji-light-webfont.woff2) format("woff2"),
+        url(fonts/fuji-light-webfont.woff) format("woff"),;
+    font-style:normal;
+}
+    p {font-family: Fuji-Bold; font-size: 20px; margin-bottom: 12px;}
+    .strip {display: flex; flex-direction: row;}
+  .flex-1 {flex: 1;}
+  .flex-2 {flex: 2;}
+  .flex-3 {flex: 3;}
+  h1 {font-family: Fuji-Bold; font-size: 20px; text-decoration: underline;}
+  ul {padding: 0px 12px; flex-wrap: wrap; display: flex;}
+  ul li.yep { flex: 1; padding: 12px; min-width: 160px; max-width: 205px;}
+  .center {text-align: center;}
+  .spin-logo > div > img {-webkit-animation: rotation 15s infinite linear;}
+  
+  .spin-logo > div > img.diff {-webkit-animation: diff 25s infinite linear;}
+  .spin-logo > div > img.diffmore {-webkit-animation: diff 20s infinite linear;}
+  .spin-logo > div {width: 100%; width: 50px;}
+  
+           @-webkit-keyframes rotation {
+		from {
+				-webkit-transform: rotateY(0deg);
+		}
+		to {
+				-webkit-transform: rotateY(359deg);
+		}
+}
+  
+  @-webkit-keyframes diff {
+		from {
+				-webkit-transform: rotate(0deg);
+		}
+		to {
+				-webkit-transform: rotate(359deg);
+		}
+}
+  
+  @-webkit-keyframes diffmore {
+		from {
+				-webkit-transform: rotateZ(0deg);
+		}
+		to {
+				-webkit-transform: rotateZ(359deg);
+		}
+}
+  
+  
+  
+</style>
+
+</head>
+
+<body>
+  
+  <?php include_once('blue.php') ?>
+  
+  <div class="strip" style="text-align: center;
+    margin-bottom: 24px; flex-direction: column;">
+    <div class="flex-1">
+    <p><a href="" style="color: white; color: white;
+    background-color: black; padding: 6px 24px 10px;">About</a></p>
     </div>
-    <div class="col-sm">
-      <div class="contenttype-title">
-        <h3>Pages</h3>
-        <a href="" class="side-text-button" data-toggle="modal" data-target="#add-content-modal"><i class="fas fa-plus-circle"></i> New page</a>
-      </div>
-      <div class="list-group">
-        <a href="#" class="list-group-item list-group-item-action">About us</a>
-        <a href="#" class="list-group-item list-group-item-action">Contact the team</a>
-        <a href="#" class="list-group-item list-group-item-action">Legal mentions</a>
-        <a href="#" class="list-group-item list-group-item-action">Our team</a>
-        <a href="#" class="list-group-item list-group-item-action">Advertising on this website</a>
-      </div>
-      <a href="list.php" class="contenttype-all-button float-right">View all pages <i class="fas fa-chevron-circle-right"></i></a>
+    
+    <div class="flex-1">
+    <p><a style="color: white; color: white;
+    background-color: black; padding: 6px 24px 10px;"href="">Shop</a></p>
     </div>
-    <div class="col-sm">
-      <h3>Comments</h3>
-      <div class="list-group">
-        <a href="#" class="list-group-item list-group-item-action">Cras justo odio</a>
-        <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
-        <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
-        <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
-        <a href="#" class="list-group-item list-group-item-action">Vestibulum at eros</a>
+  
+  </div>
+  
+  <div class="strip" style="padding: 24px; max-width: 600px;">
+    
+    <div class="flex-2">
+  <p>Maple Fortress is a studio based in the mountains of North Carolina. Work for Hopscotch Music Festival, TBS, Oxford American, Hologram Electronics and people all over the whole wide world.</p>
+      <h1>Contact</h1>
+      <p><a href="">yes@maplefortress.us</a></p>
+      
+      
       </div>
-      <a href="list.php" class="contenttype-all-button float-right">View all comments <i class="fas fa-chevron-circle-right"></i></a>
+   <!-- <div class="flex-1">
+<img src="IMG_2141.jpg">
+    </div>-->
+    <div>
+      <div style="display: flex; flex-direction: column; max-width: 500px; position: fixed; top: 24px; right: 24px;">
+    <div class="spin-logo" style="display: flex; flex-direction: row; margin: 0 auto;">
+      <div><img src="m-flip.png"></div>
+      <div><img class="diff" src="a-flip.png"></div>
+      <div><img class="diffmore" src="a-flip.png"></div>
+      <div><img class="diffmore" src="a-flip.png"></div>
+      <div><img src="a-flip.png"></div>
+    </div>
+    
+    <div class="spin-logo" style="display: flex; flex-direction: row; margin: 0 auto;">
+    <div><img src="m-flip.png"></div>
+      <div><img class="diff" src="a-flip.png"></div>
+      <div><img class="diffmore" src="a-flip.png"></div>
+      <div><img class="diffmore" src="a-flip.png"></div>
+      <div><img src="a-flip.png"></div>
+    <div><img src="a-flip.png"></div>
+      <div><img class="diffmore" src="a-flip.png"></div>
+      <div><img class="diffmore" src="a-flip.png"></div>
+    </div>
+  
+  
+  
+  </div>
+    
     </div>
   </div>
+  
+  
+  <div class="strip" style="max-width: 900px; margin: 0 auto 24px;">
+    <div class="flex-1">
+      <img src="meta-insta-1.png" alt="">
+      
+    </div>
 
-</main><!-- /.container -->
+    <div class="flex-1">
+      <img src="meta-insta-2.png" alt="">
+      <img src="meta-insta-3.png" alt="">
+     
+    </div>
+    
+    <div class="flex-1">
+       <img src="meta-insta-4.png" alt="">
+      <h1>Dog Days of the Metaverse (zine)</h1>
+      <p>40 pages</p>
+      <p>Published by U.D.L.I Editions</p>
+      <p><a href="">BUY</a></p>
+    </div>
+  
+  
+  </div>
+  
+  
+  
+  <h1 style="font-size: 30px; text-align: center; margin-bottom: 24px; text-decoration: none;">Selected Work</h1>
+  
+  <div class="strip" style="max-width: 1200px; margin: 0 auto;">
+    
+    
+    
+    <div class="flex-2">
+      <h1 class="center">Posters ></h1>
+      <ul>
+     
+        <li class="yep"><img src="yes/allred.png" alt=""></li> 
+     <li class="yep"><img src="yes/nance-FB-social.png" alt=""></li> 
+   <li class="yep"><img src="yes/gud19.png" alt=""></li>
+        <li class="yep"><img src="yes/lon-4.png" alt=""></li>
+        <li class="yep"><img src="yes/gods-cardinal-final-2018.jpg" alt=""></li>
+        
+        <li class="yep"><img src="yes/bachman-web-1200-maple-fortress.jpg" alt=""></li>
+        
+    <li class="yep"><a data-title="The Origin of Absence" data-caption="Lorem ipsum 1000/600" href="yes/betty-davis-doc-poster.jpg"><img data-title="The Origin of Absence" src="yes/betty-davis-doc-poster.jpg" alt="Betty Davis: They Say I'm Different documentary, Motorco, Durham North Carolina"></a></li>
+        
+        <li class="yep"><a data-caption="Lorem ipsum 1000/600" href="yes/WEB-lilac-june30.jpg"><img src="yes/WEB-lilac-june30.jpg" alt="Lilac Shadows poster"></a></li>
+    <li class="yep"><img src="yes/nj1.png" alt="No Joy poster"></li>
+        
+        
+        
+        <li class="yep"><img src="yes/WEB-LB-final.jpg" alt="Lightning Bolt poster"></li>
+    
+        
+    
+    <li class="yep"><img src="yes/_-WEB-RESONANCY-FINAL1.jpg" alt="Resonancy Percussion Showcase, Sun Ra, Raleigh, Ken Moshesh"></li>
+        
+        <li class="yep"><img src="yes/dead-night-cardinal-poster.jpg" alt="Grateful Dead Night, NC Grateful Dead, Boone NC, Poster"></li>
+    
+    <li class="yep"><img src="yes/hailu-raleigh-web.jpg" alt="Hailu Mergia Poster"></li>
+       
+    
+    <li class="yep"><img src="yes/busdriver-poster.png" alt="Busdriver, Deantoni Parks Poster"></li>
+   
+        <li class="yep"><img src="yes/ng-idiot-final.png" alt="Naked Gods, Idiot Glee, Murals Poster"></li>
+     
+    <li class="yep"><img src="yes/maple-fortress-patois-counselors-naked-gods.jpg" alt="Patois Counselors, Naked Gods, Black Cat, Boone, NC"></li>
+  
+        
+<li class="yep"><img src="yes/whatever-brains.png" alt="Whatever Brains Poster"></li>
+        
+      
+    </ul>
+      
+      
+      <div style="margin-top: 0px; padding: 0px 36px 36px;">
+          <h1 class="center">Exhibitions ></h1>
+          <img src="yes/gumdrops-maple-fortress.jpg">
+          </div>
+      
+    </div>
+    
+   
+      <div class="flex-2"> 
+        <div style="margin-top: 0px; padding: 0px 36px 36px;">
+         <h1 class="center">Hopscotch Music Festival ></h1>
+        <img src="hopscotch-test.png">
+          </div>
+        <div style="margin-top: 0px; padding: 0px 36px 36px;">
+          
+            <h1 class="center">Hologram Electronics ></h1>
+            <img src="yes/infinite-jets-hologram-maplefortress.jpg">
+          </div>
+        <div style="margin-top: 0px; padding: 0px 36px 36px;">
+        <h1 class="center">Oxford American ></h1>
+        <img src="insta-oa-dw-transparent.png">
+        </div>
+        <div style="margin-top: 0px; padding: 0px 36px 36px;">
+          <h1 class="center">Lily's Snack Bar / The Cardinal ></h1>
+          <img src="lilys-mf-site-test.png">
+          </div>
+        <div style="margin-top: 0px; padding: 0px 36px 36px;">
+          <h1 class="center">Naked Gods ></h1>
+          <img src="yes/Naked-Gods-Packaging-2015-front.jpg">
+          </div>
+        
+        
+      </div>
+      
 
-<?php
-  include "partials/_modal.php";
-?>
+  </div>
+</body>
 
-<?php
-  include "partials/_footer.php";
-?>
+</html>
